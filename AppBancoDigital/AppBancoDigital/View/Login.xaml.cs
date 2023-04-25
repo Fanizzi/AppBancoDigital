c3cf42930008a957c8f3b610ba3526319ccb0c08
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-04-25 15:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ AppBancoDigital/AppBancoDigital/View/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            app_logo.Source = ImageSource.FromResource("AppBancoDigital.View.app_logo.png");
+        }
+
+        private void btn_login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_esqueci_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
