cd2c588f5bb2aa1bada1b04983cf9c1d99b99e1c
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-04-26 00:58:04 -0300

    Tela Login Finalizada

--- AppBancoDigital/AppBancoDigital/View/Login.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Login.xaml.cs
@@ -24,12 +24,27 @@ namespace AppBancoDigital.View
 
         private void btn_login_Clicked(object sender, EventArgs e)
         {
+            string cpf_digitado = user_login.Text;
+            string senha_digitada = user_password.Text;
 
+            string cpf_cadastrado = "123.456.789-10";
+            string senha_cadastrada = "teste";
+
+            if (cpf_digitado == cpf_cadastrado && senha_digitada == senha_cadastrada)
+            {
+                App.Current.Properties.Add("usuario_logado", cpf_digitado);
+                App.Current.MainPage = new NavigationPage(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("Erro", "Dados incorretos!", "OK");
+            }
+           
         }
 
         private void btn_esqueci_Clicked(object sender, EventArgs e)
         {
-
+            App.Current.MainPage = new NavigationPage(new View.EsqueciSenha());
         }
     }
 }
