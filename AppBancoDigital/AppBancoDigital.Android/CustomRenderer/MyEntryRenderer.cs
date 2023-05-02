5514b6fb7e8131f666dbf94d752d4c864d7a5f81
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-05-02 15:32:20 -0300

    add CriarConta & CustomEntry

---
+++ AppBancoDigital/AppBancoDigital.Android/CustomRenderer/MyEntryRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AppBancoDigital.CustomRenderer;
+using AppBancoDigital.Droid.CustomRenderer;
+using Java.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(MyCustomeEntry), typeof(MyEntryRenderer))]
+namespace AppBancoDigital.Droid.CustomRenderer
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context) 
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null) 
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
--- AppBancoDigital/AppBancoDigital/View/CriarConta.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/CriarConta.xaml.cs
@@ -15,11 +15,17 @@ namespace AppBancoDigital.View
         public CriarConta()
         {
             InitializeComponent();
+            
         }
 
         private void btn_enviar_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_voltarLogin_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new View.Login());
         }
     }
 }
